80b25526640a5682ddd51585fda772bf8fd43538
zlv2s <706809256@qq.com>
2018-12-20 22:44:05 +0800

    add header

---
+++ src/pages/home/components/Herder.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-center">
+      <span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/varibles.scss';
+.header {
+  display: flex;
+  line-height: 0.86rem;
+  background-color: $bgColor;
+  color: #fff;
+
+  .header-left {
+    width: 0.64rem;
+    float: left;
+    .back-icon {
+      text-align: center;
+      font-size: 0.4rem;
+    }
+  }
+
+  .header-center {
+    flex: 1;
+    background-color: #fff;
+    border-radius: 0.1rem;
+    margin-top: 0.12rem;
+    margin-left: 0.2rem;
+    padding-left: 0.2rem;
+    line-height: 0.62rem;
+    height: 0.62rem;
+    color: #ccc;
+  }
+
+  .header-right {
+    text-align: center;
+    width: 1.24rem;
+    float: right;
+    .arrow-icon {
+      margin-left: -0.04rem;
+      font-size: 0.24rem;
+    }
+  }
+}
+</style>
