8558edf460256405ed83bf34f46ceabda1bdd16d
zlv2s <706809256@qq.com>
2018-12-21 13:21:06 +0800

    add icons

---
+++ src/pages/home/components/icons.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide :key="index" v-for="(page,index) in pages">
+        <div :key="item.id" class="icon" v-for="item in page">
+          <div class="icon-img">
+            <img :src="item.imgUrl" class="icon-img-content">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data() {
+    return {
+      iconList: [
+        {
+          id: '0001',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '景点门票'
+        },
+        {
+          id: '0002',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png',
+          desc: '泡温泉'
+        },
+        {
+          id: '0003',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '成都必游'
+        },
+        {
+          id: '0004',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/e3/67df61427c8e1302.png',
+          desc: '川剧变脸'
+        },
+        {
+          id: '0005',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1811/f6/e54fad3ea337b02.gif',
+          desc: '年终大促'
+        },
+        {
+          id: '0006',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png',
+          desc: '滑雪'
+        },
+        {
+          id: '0007',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '一日游'
+        },
+        {
+          id: '0008',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/54/35899492b1302802.png',
+          desc: '成都熊猫基地'
+        },
+        {
+          id: '0009',
+          imgUrl:
+            'http://img1.qunarzz.com/piao/fusion/1803/87/20656da0ac042002.png',
+          desc: '黑水彩林'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages() {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/varibles.scss';
+.icons /deep/ .swiper-container {
+  height: 0;
+  padding-bottom: 50%;
+  .icon {
+    position: relative;
+    overflow: hidden;
+    width: 25%;
+    height: 0;
+    float: left;
+    padding-bottom: 25%;
+    .icon-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0.44rem;
+      box-sizing: border-box;
+      padding: 0.1rem;
+      .icon-img-content {
+        display: block;
+        height: 100%;
+        margin: 0 auto;
+      }
+    }
+    .icon-desc {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      line-height: 0.44rem;
+      height: 0.44rem;
+      text-align: center;
+      color: $darkTextColor;
+    }
+  }
+}
+</style>
