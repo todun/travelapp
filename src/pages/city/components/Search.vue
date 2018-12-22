e1bb161445925d021ddd64a599d5fd5e00416ec8
zlv2s <706809256@qq.com>
2018-12-22 16:37:37 +0800

    add city search

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <input class="search-input" placeholder="输入城市名或拼音" type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/varibles.scss';
+.search {
+  height: 0.72rem;
+  padding: 0 0.1rem;
+  background: $bgColor;
+  .search-input {
+    box-sizing: border-box;
+    padding: 0 0.1rem;
+    height: 0.62rem;
+    line-height: 0.62rem;
+    width: 100%;
+    text-align: center;
+    border-radius: 0.06rem;
+    color: #666;
+  }
+}
+</style>
