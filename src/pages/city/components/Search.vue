63b8029f7b718f12b66b035664983e66b6bac29f
zlv2s <706809256@qq.com>
2018-12-25 09:51:27 +0800

    finish search logic

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,64 @@
 <template>
-  <div class="search">
-    <input class="search-input" placeholder="输入城市名或拼音" type="text">
+  <div>
+    <div class="search">
+      <input class="search-input" placeholder="输入城市名或拼音" type="text" v-model="keyword">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li :key="item.id" class="search-item border-bottom" v-for="item in list">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  data() {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData() {
+      return !this.list.length
+    }
+  },
+  props: ['cities'],
+  watch: {
+    keyword() {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+
+      this.timer = setTimeout(() => {
+        const res = []
+        for (const key in this.cities) {
+          this.cities[key].forEach(val => {
+            if (
+              val.spell.indexOf(this.keyword) > -1 ||
+              val.name.indexOf(this.keyword) > -1
+            ) {
+              res.push(val)
+            }
+          })
+        }
+        this.list = res
+      }, 100)
+    }
+  },
+  mounted() {
+    this.scroll = new BScroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -25,6 +77,22 @@ export default {
     text-align: center;
     border-radius: 0.06rem;
     color: #666;
+  }
+}
+.search-content {
+  z-index: 1;
+  overflow: hidden;
+  position: absolute;
+  top: 1.58rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #eee;
+  .search-item {
+    line-height: 0.62rem;
+    padding-left: 0.2rem;
+    color: #666;
+    background: #fff;
   }
 }
 </style>
